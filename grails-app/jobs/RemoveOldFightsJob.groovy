d6e6dcf148ec42db0ef0dea00738f633462e7bd4
Kevin Verhoef <kevin.verhoef@luminis.eu>
2012-10-19 12:52:46 +0200

    Added a remove old fights job

---
+++ grails-app/jobs/RemoveOldFightsJob.groovy
@@ -0,0 +1,25 @@
+import game.FightFactoryService
+import game.Fight
+
+/**
+ * Fights older then x amount of time should be destroyed so the
+ * don't remain forever in memory.
+ */
+class RemoveOldFightsJob {
+
+    FightFactoryService fightFactoryService
+
+    static triggers = {
+        simple name:'removeOldFightsJob', startDelay:30 * 1000, repeatInterval: 5 * 60 * 1000
+    }
+
+    def concurrent = false
+
+    def execute() {
+        FightFactoryService.fights.findAll{ new Date()-1 > it.createDate  }.each { Fight fight ->
+            println "Destroy fight ${fight}"
+            fightFactoryService.endFight(fight)
+        }
+    }
+
+}
