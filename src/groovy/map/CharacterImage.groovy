a1fea55226c370cf4a806552e16ee6e3954ae295
Kevin Verhoef <kevin.verhoef@luminis.eu>
2013-07-18 09:38:02 +0200

    CharacterImages

--- src/groovy/map/CharacterImage.groovy
+++ src/groovy/map/CharacterImage.groovy
@@ -1,5 +1,5 @@
 package map
 
 enum CharacterImage {
-    player, band, girl, girl2, guard, person1, person2, person3, person4
+    player, band, girl, girl2, guard, person1, person2, person3, person4, person5, person6, person7, person8, person9, person10, person11
 }
